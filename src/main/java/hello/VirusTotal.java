867ba3d2384ed993b8e1e6494afdf9d4b8e81586
Mateus Lima <mateuslimadev@gmail.com>
2022-07-11 21:18:02 -0300

    first commit

---
+++ src/main/java/hello/VirusTotal.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class VirusTotal
+{
+}
+
+ class VirusTotalUrl
+{
+ private String url;
+
+ void setUrl (String url)
+ {
+     if(url!=null)
+     {
+         this.url=url;
+     }
+ }
+
+    String getUrl ()
+    {
+        return this.url;
+    }
+}
